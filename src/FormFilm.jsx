55c9e1ac59b026730621b530eb30b41eef166aa8
Remi <aligastudioopas@gmail.com>
2019-04-28 22:51:29 +0200

    indentation

--- src/FormFilm.jsx
+++ src/FormFilm.jsx
@@ -1,112 +1,112 @@
-  import React, { Component } from 'react';
+import React, { Component } from 'react';
 
 
 
-  class FormFilm extends Component {
+class FormFilm extends Component {
 
 
 
-      constructor(props) {
-          super(props);
-          this.state = {
-            name: '',
-            poster: '',
-            comment: '',
-          }
-          this.onChange = this.onChange.bind(this);
-          this.submitForm = this.submitForm.bind(this);
-        }
-
-        onChange(e) {
-          this.setState({
-            [e.target.name]: e.target.value,
-          });
-        }
-
-
-
-        submitForm(e) { 
-          
-          const config = {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(this.state),
-            };
-          
-          const url = "http://campus-bordeaux.ovh:3001/api/quests/movies/";
-        
-
-          fetch(url, config)
-          .then(res => res.json())
-          .then(res => {
-            if (res.error) {
-              alert(res.error);
-            } else {
-              alert(`Film ajouté avec l'ID ${res}!`);
-            }
-          }).catch(e => {
-            console.error(e);
-            alert('Erreur lors de l\'ajout d\'un film');
-          });
-          e.preventDefault();
-          }
-      
-
-
-      render () {
-
-      return (
-
-          <div className="FormFilm">
-
-          <h1>Saisie d'un film</h1>
-        
-          <form onSubmit={this.submitForm}>
-            <fieldset>
-              <legend>Informations</legend>
-              <div className="form-data">
-                <label htmlFor="name">Nom du film</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  onChange={this.onChange}
-                  value={this.state.name}
-                />
-              </div>
-        
-              <div className="form-data">
-                <label htmlFor="url">URL poster</label>
-                <input
-                  type="textarea"
-                  id="poster"
-                  name="poster"
-                  onChange={this.onChange}
-                  value={this.state.poster}
-                />
-              </div>
-        
-              <div className="form-data">
-                <label htmlFor="commentaire">Commentaire</label>
-                <input
-                  type="textarea"
-                  id="comment"
-                  name="comment"
-                  onChange={this.onChange}
-                  value={this.state.comment}
-                />
-              </div>
-              <hr />
-              <div className="form-data">
-                <input type="submit" value="Envoyer" />
-              </div>
-            </fieldset>
-          </form>
-          </div>
-
-      )
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      poster: '',
+      comment: '',
+    }
+    this.onChange = this.onChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
   }
-  export default FormFilm;
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+
+
+  submitForm(e) {
+
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    };
+
+    const url = "http://campus-bordeaux.ovh:3001/api/quests/movies/";
+
+
+    fetch(url, config)
+      .then(res => res.json())
+      .then(res => {
+        if (res.error) {
+          alert(res.error);
+        } else {
+          alert(`Film ajouté avec l'ID ${res}!`);
+        }
+      }).catch(e => {
+        console.error(e);
+        alert('Erreur lors de l\'ajout d\'un film');
+      });
+    e.preventDefault();
+  }
+
+
+
+  render() {
+
+    return (
+
+      <div className="FormFilm">
+
+        <h1>Saisie d'un film</h1>
+
+        <form onSubmit={this.submitForm}>
+          <fieldset>
+            <legend>Informations</legend>
+            <div className="form-data">
+              <label htmlFor="name">Nom du film</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                onChange={this.onChange}
+                value={this.state.name}
+              />
+            </div>
+
+            <div className="form-data">
+              <label htmlFor="url">URL poster</label>
+              <input
+                type="textarea"
+                id="poster"
+                name="poster"
+                onChange={this.onChange}
+                value={this.state.poster}
+              />
+            </div>
+
+            <div className="form-data">
+              <label htmlFor="commentaire">Commentaire</label>
+              <input
+                type="textarea"
+                id="comment"
+                name="comment"
+                onChange={this.onChange}
+                value={this.state.comment}
+              />
+            </div>
+            <hr />
+            <div className="form-data">
+              <input type="submit" value="Envoyer" />
+            </div>
+          </fieldset>
+        </form>
+      </div>
+
+    )
+  }
+}
+export default FormFilm;
